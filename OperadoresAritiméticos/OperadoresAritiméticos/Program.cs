feb9583c4f7634da7ebba4f697bee4e31d5350d2
Felipe Pontes dos Santos <pontesfelipe835@gmail.com>
2022-09-16 16:40:53 -0300

    C#

---
+++ OperadoresAritiméticos/OperadoresAritiméticos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresAritiméticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeroQualquer = 20 + 20;
+            int numeroMult = 10 * 90;
+            float numeroDiv = 5.0f / 2;
+
+             // 6 / 2 =>  // int
+            // 5 / 2 => // decimal // float 
+
+
+            Console.WriteLine(numeroQualquer);
+            Console.WriteLine(numeroMult);
+            Console.WriteLine(numeroDiv);
+
+
+            Console.ReadLine();
+        }
+    }
+}
