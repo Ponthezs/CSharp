2fc6f0d590a581ff3c59241cd9f483e770cb5f99
Felipe Pontes dos Santos <pontesfelipe835@gmail.com>
2022-09-23 17:33:26 -0300

    C#

---
+++ Condicionais/Condicionais/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (1 > 2)
+            {
+                Console.WriteLine("É verdade");
+            } else
+            {
+                Console.WriteLine("È mentira");
+            }
+               
+            Console.ReadLine();
+        }
+    }
+}
