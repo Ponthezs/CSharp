b8e1663454e66e4befbab7fa685ab875a7c7a957
Felipe Pontes dos Santos <pontesfelipe835@gmail.com>
2022-09-29 17:40:36 -0300

    C#

--- Condicionais/Condicionais/Program.cs
+++ Condicionais/Condicionais/Program.cs
@@ -12,19 +12,33 @@ namespace Condicionais
         static void Main(string[] args)
         {
 
-            int a = 20;
-            int b = 70;
-            int c = 70;
+            var a = 10;
+            int b = 20;
+            int c = 200;
+
+            // (E) && e || (OU/OR)
+
+            //TRUE && TRUE =  TRUE
+            //FALSE && FALSE = FALSE
+            //FALSE && TRUE = FALSE
+            //TRUE && FALSE = FALSE
+
+           //TRUE || FALSE = TRUE, TENDO UM VERDADEIRO SEMPRE SERÁ VERDADEIRO PRECISA DE UMA CONDIÇÃO TRUE, CASO N SEJA SERÁ FALSE
+
+           //TRUE || TRUE =  TRUE
+           //FALSE || TRUE = TRUE
+           //TRUE || FALSE = TRUE
+           //FALSE || FALSE = FALSE
 
 
-            if (a == b)
+            if (a < b || a > c) // SE
             {
                 Console.WriteLine("É verdade");
             } else if (c < 20)
             {
                 Console.WriteLine("Executou Elseif");
             }
-            else
+            else // SE NÃO
             {
                 Console.WriteLine("È mentira");
             }
