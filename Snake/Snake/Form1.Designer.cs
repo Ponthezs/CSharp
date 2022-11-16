d9d16ab1c55de602e5e818aeb4a2302079d9f92e
felipe.pontes <pontesfelipe835@gmail.com>
2022-11-16 17:55:51 -0300

    C#

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -28,24 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PnTela = new System.Windows.Forms.Panel();
+            this.lbPontos = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarJogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Frame = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // PnTela
+            // 
+            this.PnTela.Location = new System.Drawing.Point(0, 92);
+            this.PnTela.Name = "PnTela";
+            this.PnTela.Size = new System.Drawing.Size(448, 327);
+            this.PnTela.TabIndex = 0;
+            // 
+            // lbPontos
+            // 
+            this.lbPontos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPontos.Location = new System.Drawing.Point(0, 64);
+            this.lbPontos.Name = "lbPontos";
+            this.lbPontos.Size = new System.Drawing.Size(448, 25);
+            this.lbPontos.TabIndex = 0;
+            this.lbPontos.Text = "PONTOS:";
+            this.lbPontos.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(448, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Menu
+            // 
+            this.Menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarJogoToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.sairToolStripMenuItem});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(50, 20);
+            this.Menu.Text = "Menu";
+            // 
+            // iniciarJogoToolStripMenuItem
+            // 
+            this.iniciarJogoToolStripMenuItem.Name = "iniciarJogoToolStripMenuItem";
+            this.iniciarJogoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.iniciarJogoToolStripMenuItem.Text = "Iniciar jogo";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 399);
+            this.ClientSize = new System.Drawing.Size(448, 420);
+            this.Controls.Add(this.lbPontos);
+            this.Controls.Add(this.PnTela);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Snake ll";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel PnTela;
+        private System.Windows.Forms.Label lbPontos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Menu;
+        private System.Windows.Forms.ToolStripMenuItem iniciarJogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer Frame;
     }
 }
 
---
+++ Snake/Snake/snake.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class snake
+    {
+        public int length { get; private set; }
+        public Point[] Location { get; private set; }
+
+        public snake ()
+        {
+            Location = new Point[28 * 28];
+            Reset();
+        }
+
+        public void Reset()
+        {
+            length = 5;
+            for (int i = 0; i < length; i++)
+            {
+                Location[i].X = 12;
+                Location[i].Y = 12;
+            }
+        }
+
+        public void Follow()
+        {
+            for(int i = length -1; i >0; i--)
+            {
+                Location[i] = Location[i - 1];
+            }
+        }
+
+        public void Up() 
+        {
+            Follow();
+            Location[0].Y--;
+            if (Location[0].Y < 0)
+            {
+                Location[0].Y += 28;
+            }
+        }
+
+        public void Down() 
+        {
+            Follow();
+            Location[0].Y++;
+            if (Location[0].Y < 27)
+            {
+                Location[0].Y -= 28;
+            }
+        }
+            
+        public void Left()
+        {
+            Follow();
+            Location[0].X--;
+            if (Location[0].X < 0)
+            {
+                Location[0].X += 28;
+            }
+        }
+
+        public void Right() 
+        {
+            Follow();
+            Location[0].X++;
+            if (Location[0].X > 27)
+            {
+                Location[0].X += 28;
+            }
+        }
+
+
+
+
+
+    }
+}
